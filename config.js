5d8470b2d68c334779377ab3b53f4624c1b72780
James Elías <jamiees2@gmail.com>
2013-04-12 18:37:08 +0000

    Fixed mongo connection

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ exports.dev = {
 exports.prod = {
     port: process.env.PORT || 14858, // Nodester port
     client_port: 80, // Websockets talk on port 80 on Nodester, regardless of listen port
-    mongoose_auth: process.env.MONGODB_URL ||  'mongodb://mongodb@localhost/subway'
+    mongoose_auth: process.env.MONGOOSE_AUTH
 }
 
 exports.misc = {
