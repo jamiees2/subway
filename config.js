c32c9415153bfe7b858f1a2c4ca4296792919117
James Elías <jamiees2@gmail.com>
2013-04-13 17:52:26 +0000

    fixed database

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 exports.dev = {
     port: process.env.PORT || 3000,
     client_port: process.env.PORT || 3000,
-    mongoose_auth: process.env.MONGODB_URL || 'mongodb://mongodb@localhost/subway'
+    mongoose_auth: process.env.MONGOLAB_URI || 'mongodb://mongodb@localhost/subway'
 }
 
 exports.prod = {
     port: process.env.PORT || 14858, // Nodester port
     client_port: 80, // Websockets talk on port 80 on Nodester, regardless of listen port
-    mongoose_auth: process.env.MONGOOSE_AUTH
+    mongoose_auth: process.env.MONGOLAB_URI
 }
 
 exports.misc = {
